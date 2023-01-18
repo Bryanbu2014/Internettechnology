144408b0920c1021fbbb38d26c401f2f6b281427
Wen Bin Bu <wen.bu@stud.th-deg.de>
2023-01-18 22:12:38 +0100

    Add file

---
+++ staticserver-mit-webseite.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+//Wir laden contents aus dem public Ordner
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+
+var server = app.listen(8081, function(){
+    var port = server.address().port;
+    console.log("Server wird gestartet http://localhost:%s", port);
+});
